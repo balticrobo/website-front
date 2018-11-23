313133572a012081196a333d89431ed0e0541f50
KarolKonopka <KarolKonopka00@gmail.com>
2018-11-23 21:09:08 +0100

    Clean app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <div className='App'>
-    <header className='App-header'>
-      <img src={logo} className='App-logo' alt='logo' />
-      <p>
-        Edit
-        {' '}
-        <code>src/components/App/App.jsx</code>
-        {' '}
-and save to reload.
-      </p>
-      <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-        Learn React
-      </a>
-    </header>
+  <div>
+    <h1>Baltic Robo</h1>
   </div>
 );
 
