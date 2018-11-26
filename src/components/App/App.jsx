0a2d05ffad39b6007e31ebb23a8fd3a9a291d1e9
KarolKonopka <KarolKonopka00@gmail.com>
2018-11-26 21:29:04 +0100

    Add navigation bar with logo to subsites

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,70 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter, Route, Link, Switch
+} from 'react-router-dom';
+import logo from './BBR_logo_sam_tekst.svg';
+
+const SiteNav = () => (
+  <ul>
+    <li>
+      <Link to='/'>Główna</Link>
+    </li>
+    <li>
+      <Link to='/rules'>Regulaminy</Link>
+    </li>
+    <li>
+      <Link to='/partners'>Partnerzy</Link>
+    </li>
+  </ul>
+);
+
+const Index = () => (
+  <div>
+    <span>Główna</span>
+  </div>
+);
+
+const Rules = () => (
+  <div>
+    <span>Regulaminy</span>
+  </div>
+);
+
+const Partners = () => (
+  <div>
+    <span>Partnerzy</span>
+  </div>
+);
+
+const NoMatch = () => (
+  <div>
+    <span>404</span>
+  </div>
+);
 
 const App = () => (
-  <div>
-    <h1>Baltic Robo</h1>
-  </div>
+  <BrowserRouter>
+    <div>
+      <div className='header-bar'>
+        <div id='header-container'>
+          <img src={logo} id='logo' alt='Bałtyckie Bitwy Robotów' />
+          <SiteNav />
+        </div>
+      </div>
+      <div className='content'>
+        <div id='content-container'>
+          <Switch>
+            <Route path='/' exact component={Index} />
+            <Route path='/rules' component={Rules} />
+            <Route path='/partners' component={Partners} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </div>
+      <div className='footer' />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
