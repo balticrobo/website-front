e87e84f4a49ca6be53004beaea1f78309ba0bd37
KarolKonopka <KarolKonopka00@gmail.com>
2018-12-03 17:42:38 +0100

    Fix routes and content display

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,45 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import {
   BrowserRouter, Route, Link, Switch
 } from 'react-router-dom';
 import logo from './BBR_logo_sam_tekst.svg';
 
+const TitleHeader = ({ headerText }) => (
+  <div className='title-block'>
+    <span>
+      {headerText}
+    </span>
+  </div>
+);
+
+TitleHeader.propTypes = {
+  headerText: PropTypes.string.isRequired
+};
+
 const SiteNav = () => (
-  <ul>
-    <li>
-      <Link to='/'>Główna</Link>
-    </li>
-    <li>
-      <Link to='/rules'>Regulaminy</Link>
-    </li>
-    <li>
-      <Link to='/partners'>Partnerzy</Link>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li>
+        <Link to='/'>Główna</Link>
+      </li>
+      <li>
+        <Link to='/event/rules'>Regulaminy</Link>
+      </li>
+      <li>
+        <Link to='/partners'>Partnerzy</Link>
+      </li>
+    </ul>
+  </div>
 );
 
 const Index = () => (
   <div>
-    <span>Główna</span>
+    <TitleHeader headerText='Główna' />
   </div>
 );
 
 const Rules = () => (
   <div>
-    <span>Regulaminy</span>
+    <TitleHeader headerText='Regulaminy' />
   </div>
 );
 
 const Partners = () => (
   <div>
-    <span>Partnerzy</span>
+    <TitleHeader headerText='Partnerzy' />
   </div>
 );
 
 const NoMatch = () => (
   <div>
-    <span>404</span>
+    <TitleHeader headerText='404' />
   </div>
 );
 
@@ -48,7 +63,7 @@ const App = () => (
     <div>
       <div className='header-bar'>
         <div id='header-container'>
-          <img src={logo} id='logo' alt='Bałtyckie Bitwy Robotów' />
+          <Link to='/'><img src={logo} id='logo' alt='Bałtyckie Bitwy Robotów' /></Link>
           <SiteNav />
         </div>
       </div>
@@ -56,7 +71,7 @@ const App = () => (
         <div id='content-container'>
           <Switch>
             <Route path='/' exact component={Index} />
-            <Route path='/rules' component={Rules} />
+            <Route path='/event/rules' component={Rules} />
             <Route path='/partners' component={Partners} />
             <Route component={NoMatch} />
           </Switch>
